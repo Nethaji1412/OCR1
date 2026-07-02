b309af5409fca2b1e0ef3bfe2ba6740233c94c62
Nethaji Ram <nethajitvm150@gmail.com>
2026-07-03 00:03:03 +0530

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -21,7 +21,7 @@ import sys
 # except ImportError:
 #     print("Installing opencv-python...")
 #     os.system("pip install opencv-python-headless")
-import cv2
+# import cv2
 
 #Import custom modules (cloud-compatible)
 try:
